56e6ec7bcab985cab07450cf07517831f0612698
Egor <sychyov1991@mail.ru>
2022-02-07 19:55:16 +0600

    Add project files.

---
+++ SeparateToPackage/Data.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace SeparateToPackage
+{
+    public class Data
+    {
+        public static Dictionary<string, string> GetData()
+        {
+            var data = new Dictionary<string, string>();
+            data.Add("1","val");
+            data.Add("2", "val");
+            data.Add("3", "val");
+            data.Add("4", "val");
+            data.Add("5", "val");
+            data.Add("6", "val");
+            data.Add("7", "val");
+            data.Add("8", "val");
+            data.Add("9", "val");
+            data.Add("10", "val");
+            data.Add("11", "val");
+            data.Add("12", "val");
+            data.Add("13", "val");
+            data.Add("14", "val");
+            data.Add("15", "val");
+            data.Add("16", "val");
+
+            return data;
+        }
+    }
+}
---
+++ SeparateToPackage/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace SeparateToPackage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = Data.GetData().ToList();
+            var total = data.Count;
+            var packageSize = 5;
+
+            do
+            {
+                var package = data.Skip(data.Count - total).Take(packageSize);
+                var sb = new StringBuilder("");
+
+                foreach (var i in package)
+                {
+                    sb.AppendFormat("update table set val = {0} where KEY = '{1}'  ", i.Value, i.Key);
+                }
+
+                total -= packageSize;
+                Console.WriteLine(sb.ToString());
+                //execute query sb.ToString()
+                Console.WriteLine(".......................");
+            } while (total > 0);
+
+            Console.ReadKey();
+        }
+    }
+}
